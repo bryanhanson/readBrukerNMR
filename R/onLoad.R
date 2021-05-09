ecc7fe8f25302086eb0de1f376c885feb0b7e4a1
Bryan <hanson@depauw.edu>
2021-05-09 16:15:41 -0700

    bringing up to date

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,10 +1,20 @@
 #'
 #' @noRd
-#' @importFrom reticulate import
+#' @importFrom reticulate import py_module_available
 #'
 .onLoad <- function(libname, pkgname) {
 
+  install_instructions <- paste(
+    "\n\n",
+    "\nYou need to install the nmrglue Python module as follows:\n",
+    "\tlibrary('reticulate')\n",
+    "\tuse_virtualenv('r-reticulate')\n",
+    "\tpy_install('nmrglue', envname = 'r-reticulate')\n",
+    "\tand when the install process is done, try again with:\n",
+    "\tlibrary('readBrukerNMR)\n\n")
+
+  if (!py_module_available("nmrglue")) stop(install_instructions)
+
   # Import nmrglue to global environment (lazily)
   ng <<- import("nmrglue", delay_load = TRUE)
-
 }
--- R/readBrukerNMR-package.R
+++ R/readBrukerNMR-package.R
@@ -3,7 +3,7 @@
 #'
 #' Read in Bruker NMR (Nuclear Magnetic Resonance) data.  Currently only reads processed data.
 #' Built around the Python ['nmrglue'](https://github.com/jjhelmus/nmrglue/) module by JJ Helmus.
-#' Includes a data set from [BMRB](https://bmrb.io/) with permission for demonstration and testing.
+#' Includes a data set from [BMRB](https://bmrb.io/) for demonstration and testing.
 #' The included data are 1H and 13C NMR spectra of the disaccharide melibiose.  These spectra are
 #' a portion of the
 #' [bmse000233 data set](https://bmrb.io/metabolomics/mol_summary/show_data.php?id=bmse000233)
